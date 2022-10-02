e5f695d2b2d6bd65706d50b45ebdf4f32a9a99c8
Hiruy Melaku <95427259+Hiruy3@users.noreply.github.com>
2022-10-02 16:54:15 +0300

    Add files via upload

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,7 @@
--- Configuration of database for work
+-- prepares a MySQL server for the project
 
--- Create database named hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- use the database
-USE `hbnb_test_db`;
-
--- Create user of the database
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- give all the user's properties to the database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- let the user see the perfomance data
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
